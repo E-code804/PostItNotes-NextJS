ef5ea396789c311d6dac4f7dd41d1fd7239b865e
E-code804 <erikpfeffer5@gmail.com>
2023-08-06 22:49:05 -0400

    Fixed null issue w/ TopicsList.

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -23,24 +23,25 @@ const TopicsList = async () => {
 
   return (
     <>
-      {topics.map((topic) => (
-        <div
-          key={topic._id}
-          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
-        >
-          <div>
-            <h2 className="font-bold text-2xl">{topic.title}</h2>
-            <div>{topic.description}</div>
-          </div>
+      {topics &&
+        topics.map((topic) => (
+          <div
+            key={topic._id}
+            className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+          >
+            <div>
+              <h2 className="font-bold text-2xl">{topic.title}</h2>
+              <div>{topic.description}</div>
+            </div>
 
-          <div className="flex gap-2">
-            <RemoveBtn id={topic._id} />
-            <Link href={`/editTopic/${topic._id}`}>
-              <HiPencilAlt size={24} />
-            </Link>
+            <div className="flex gap-2">
+              <RemoveBtn id={topic._id} />
+              <Link href={`/editTopic/${topic._id}`}>
+                <HiPencilAlt size={24} />
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 };
